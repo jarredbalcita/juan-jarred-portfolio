1f3774df2c79b0d23c718c9be77de6bf43d9b031
jarred-hub <juanjarred@gmail.com>
2025-08-25 17:55:04 +0400

    test again

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ const initCustomCursor = () => {
     let currentY = 0;
     let animationFrame;
     let initialized = false;
-    const lerp = 0.22;
+    const lerp = 0.6;
 
     const animateCursor = () => {
         const dx = mouseX - currentX;
